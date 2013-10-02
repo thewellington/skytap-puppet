4d1f6ab92058cfef31fbcd3ff4b3ed98019a97bb
W. S. Wellington <bwellington@fulcrum.net>
2013-10-02 10:30:46 -0700

    parse parse parse
    Signed-off-by: W. S. Wellington <bwellington@fulcrum.net>

--- test.py
+++ test.py
@@ -2,36 +2,35 @@
 #
 #
 
-
+# import modules
 import json
 import urllib
 import urllib2
+import hashlib
 from pprint import pprint
 
-# Step 1, get gateway address
+def getJSON(data):
+   result = json.load(data)
+   return result
 
-# Step 2, get metadata from gateway
-url = "http://192.168.1.254/skytap"
-#req = urllib2.Request(url)
-with urllib2.urlopen(url) as response:
-  new_metadata = getJson(response)
-  print NEW DATA
+
+# Main Function
+def main ():
+
+# get current metadata from skytap
+  url = "http://192.168.1.254/skytap"
+  response = urllib2.urlopen(url)
+  new_metadata = getJSON(response)
   pprint (new_metadata)
 
-# Step 3, get stored metadata
-with open('samplejson.txt') as infile:
-  old_metadata = json.load(infile)
-  #pprint (data)
-  pprint (old_metadata["id"])
-  pprint (old_metadata["interfaces"][0]['ip'])
+# get stored metadata fron disk
+  with open('samplejson.txt') as infile:
+    old_metadata = getJSON(infile)
+    #data = json.load(infile)
+    #pprint (data)
+    pprint (old_metadata["id"])
+    pprint (old_metadata["interfaces"][0]['ip'])
 
-# Step 4, compare new and old metadata
-
-
-
-
-
-# json Load
-def getJson(input):
-  data = json.load(input)
-  return data
+# Call main()
+if __name__ == '__main__':
+  main()
