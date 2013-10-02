2338213bbf15745a14f30ca14bd38c06c78d701b
W. S. Wellington <bwellington@fulcrum.net>
2013-10-02 12:16:28 -0700

    an idea begins
    Signed-off-by: W. S. Wellington <bwellington@fulcrum.net>

--- test.py
+++ test.py
@@ -9,27 +9,42 @@ import urllib2
 import hashlib
 from pprint import pprint
 
+# set variables
+url = "http://192.168.1.254/skytap"
+json_path = 'samplejson.txt'
+
+# build functions
 def getJSON(data):
-   result = json.load(data)
-   return result
+  result = json.load(data)
+  return result
+
+def get_current_metadata():
+  response = urllib2.urlopen(url)
+  current_metadata = getJSON(response)
+  result = current_metadata["id"]
+  print result
+
+def get_stored_metadata():
+  with open(json_path) as infile:
+    stored_metadata = getJSON(infile)
+    result = stored_metadata["id"]
+    print result
 
 
+###############################################################################    
 # Main Function
 def main ():
 
-# get current metadata from skytap
-  url = "http://192.168.1.254/skytap"
-  response = urllib2.urlopen(url)
-  new_metadata = getJSON(response)
-  pprint (new_metadata)
+# if the file at json_path exists:
 
-# get stored metadata fron disk
-  with open('samplejson.txt') as infile:
-    old_metadata = getJSON(infile)
-    #data = json.load(infile)
-    #pprint (data)
-    pprint (old_metadata["id"])
-    pprint (old_metadata["interfaces"][0]['ip'])
+  current_metadata = get_current_metadata()
+  current_id = 
+
+# compare the values
+  if (current_id == stored_id):
+    print ('OK!')
+  else:
+    print ('Something is wrong!')
 
 # Call main()
 if __name__ == '__main__':
